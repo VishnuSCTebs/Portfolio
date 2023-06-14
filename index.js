ba7dee3bd3fac0ff802f087203decd498d0ad08c
Vishnu Satish <vishnu.satish@ctebs.com>
2023-06-14 15:15:42 +0530

    add mobile ui

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("working");
+const navLinks=document.querySelectorAll(".app__intro__navigation__link")
+var active=0;
+
+
+for(var i=0;i<navLinks.length;i++){
+    if(i==active){
+        
+    }
+}
