b21956c5e6824fcd062ec4fd2bf16e021f2fa768
Vishnu Satish <vishnu.satish@ctebs.com>
2023-06-14 16:31:58 +0530

    add gradient cursor

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const navLinks=document.querySelectorAll(".app__intro__navigation__link")
-var active=0;
+const gradientCursor=document.getElementById("gradient-cursor")
+// const navLinks=document.querySelectorAll(".app__intro__navigation-link")
+
+// // console.log(gradientCursor);
+// var active=0;
+
+// console.log(navLinks)
+// for(var i=0;i<navLinks.length;i++){
+
+//     navLinks[i].addEventListener("click",()=>{
+    
+            
+//     })
+    
+// }
+
+// console.log(document.getElementById("gradient-cursor"))
+
+window.addEventListener("mousemove",(e)=>{
+    const x=e.clientX + document.body.scrollLeft
+    const y=e.clientY + document.body.scrollTop
 
 
-for(var i=0;i<navLinks.length;i++){
-    if(i==active){
-        
-    }
-}
+
+    gradientCursor.style.transform=`translate3d(${x}px,${y}px,0)`;
+})
+
